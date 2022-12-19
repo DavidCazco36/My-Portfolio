d7d7435f391905879a4a7470b24503b05a614e7d
DavidCazco36 <davidcazco11q@gmail.com>
2022-12-19 16:48:09 -0500

    Added an interactive menu

---
+++ script.js
@@ -0,0 +1,25 @@
+let is_on = Boolean
+
+document.getElementsByClassName('menu-icon')[0].addEventListener('click', function () {
+   
+    if (is_on === true) {
+        document.getElementsByClassName('nav-bar')[0].style.display = 'none'
+        document.getElementsByTagName('body')[0].style.overflowY = 'scroll'
+        is_on = false
+        console.log (is_on)
+    }
+
+    else {
+    document.getElementsByClassName('nav-bar')[0].style.display = 'flex'
+    document.getElementsByTagName('body')[0].style.overflowY = 'hidden'
+    is_on = true
+    console.log (is_on)
+    }
+})
+
+function close_button() {
+    console.log('test')
+    document.getElementsByClassName('nav-bar')[0].style.display = 'none'
+    document.getElementsByTagName('body')[0].style.overflow = 'scroll'
+    is_on = false
+}
