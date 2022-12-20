89e8def10e4833ed79784d0fd6d07ef0a1e9b0c5
DavidCazco36 <davidcazco11q@gmail.com>
2022-12-20 11:08:43 -0500

    Fixed what the code reviewer told me

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ let is_on = Boolean
 document.getElementsByClassName('menu-icon')[0].addEventListener('click', function () {
    
     if (is_on === true) {
-        document.getElementsByClassName('nav-bar')[0].style.display = 'none'
-        document.getElementsByClassName('logo')[0].style.display = 'flex'
-        document.getElementsByClassName('header-container')[0].style.justifyContent ='space-between'
+        document.getElementsByClassName('nav-bar')[0].style.display = 'none';
+        document.getElementsByClassName('logo')[0].style.display = 'flex';
+        document.getElementsByClassName('header-container')[0].style.justifyContent ='space-between';
+        document.getElementsByClassName('logo')[0].style.display = 'flex';
+        document.getElementsByClassName('burger-img')[0].src = 'media/Icon - Menu.png';
         is_on = false
         console.log (is_on)
     }
 
     else {
-    document.getElementsByClassName('nav-bar')[0].style.display = 'flex'
-    document.getElementsByClassName('logo')[0].style.display = 'none'
-    document.getElementsByClassName('header-container')[0].style.justifyContent ='flex-end'
+    document.getElementsByClassName('nav-bar')[0].style.display = 'flex';
+    document.getElementsByClassName('logo')[0].style.display = 'none';
+    document.getElementsByClassName('header-container')[0].style.justifyContent ='flex-end';
+    document.getElementsByClassName('burger-img')[0].src =  'media/Icon - Cancel.png';
     is_on = true
     console.log (is_on)
     }
